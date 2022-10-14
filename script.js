c5cf7db1bf8dc1e78dc21cabfb5da771cc8d317d
tomifuchi <tomifuchi@yahoo.com>
2022-10-14 15:21:40 +0700

    Active box in progress

--- script.js
+++ script.js
@@ -10,8 +10,69 @@ pd_img_variation.forEach( img => {
         delete document.querySelector('.product-img-detail li[data-active-image]').dataset.activeImage;
         img.dataset.activeOverlay = true;
         pd_img_detail[currentIndex].dataset.activeImage = true;
+        pd_img_detail[currentIndex].scrollIntoView(); //Huehuehuehue
     })
 });
+
+const pd_img_detail_button_next = document.querySelector('.product-img-detail button[data-button-next]');
+const pd_img_detail_button_prev = document.querySelector('.product-img-detail button[data-button-prev]');
+function scroll_img_carousel(step) {
+   const active_img = document.querySelector('.product-img-detail li[data-active-image]');
+   let currentIndex = [...pd_img_detail].indexOf(active_img);
+   let nextIndex;
+
+   if (currentIndex + step < 0) {
+    nextIndex = pd_img_detail.length - 1;
+   } else if ( currentIndex + step > pd_img_detail.length - 1) {
+    nextIndex = 0;
+   }
+   else {
+    nextIndex = currentIndex + step;
+   }
+
+   delete document.querySelector('.product-img-variation li[data-active-overlay]').dataset.activeOverlay;
+   delete document.querySelector('.product-img-detail li[data-active-image]').dataset.activeImage;
+   pd_img_variation[nextIndex].dataset.activeOverlay = true;
+   pd_img_detail[nextIndex].dataset.activeImage = true;
+   pd_img_detail[nextIndex].scrollIntoView();
+}
+
+pd_img_detail_button_next.addEventListener('click', () => {scroll_img_carousel(1);});
+pd_img_detail_button_prev.addEventListener('click', () => {scroll_img_carousel(-1);});
+
+
+window.addEventListener('keyup', event => {
+    switch(event.key) {
+        case 'ArrowRight':
+            scroll_img_carousel(1);
+            break;
+        case 'ArrowLeft':
+            scroll_img_carousel(-1);
+            break;
+    }
+});
+
+////Now for the hand scrolling
+//const observer = new IntersectionObserver( entries => {
+//    entries.forEach( entry => {
+//    });
+//    //delete document.querySelector('.product-img-variation li[data-active-overlay]').dataset.activeOverlay;
+//    //delete document.querySelector('.product-img-detail li[data-active-image]').dataset.activeImage;
+//
+//    let nextIndex = [...pd_img_detail].indexOf(entries[0].target); 
+//    console.log(nextIndex);
+//    //pd_img_variation[nextIndex].dataset.activeOverlay = true;
+//    //pd_img_detail[nextIndex].dataset.activeImage = true;
+//
+//}, {
+// root: document.querySelector('.product-img-detail ul'),
+//});
+//
+//pd_img_detail.forEach( img => {
+//    observer.observe(img);
+//});
+
+
 
 //Product detail area
 //Button for the add to cart and incrementing the 
