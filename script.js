cc545a548126761299aae4472710a96bf5760100
tomifuchi <tomifuchi@yahoo.com>
2022-10-14 08:38:32 +0700

    Component in progress

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Product image area
-const pd_img_detail = document.querySelectorAll('.product-img-detail li');
-const pd_img_variation = document.querySelectorAll('.product-img-variation li');
 
 //Navigating the using thumbnail image below main image
+const pd_img_detail = document.querySelectorAll('.product-img-detail li');
+const pd_img_variation = document.querySelectorAll('.product-img-variation li');
 pd_img_variation.forEach( img => {
     img.addEventListener('click', () => {
         let currentIndex = [...pd_img_variation].indexOf(img);
@@ -12,37 +12,6 @@ pd_img_variation.forEach( img => {
         pd_img_detail[currentIndex].dataset.activeImage = true;
     })
 });
-
-
-
-
-//Zooming into centering the product image section in center of screen
-const toggle_activeOverlay = document.querySelector('.body-overlay'); //activeBodyOverlay;
-const toggle_pd_imgOverlay = document.querySelector('.product-img'); //activeOverlay;
-
-//Besure to pass in a node
-function toggle_attrib(element, attrib){
-    console.log(element.hasAttribute(attrib));
-    if(element.hasAttribute(attrib)) {
-        element.removeAttribute(attrib);
-    } else
-        element.setAttribute(attrib,'');
-    console.log(element.getAttribute(attrib));
-}
-
-pd_img_detail.forEach( img => {
-    img.addEventListener('click', () => {
-        document.body.classList.toggle('body-fixed');
-        toggle_attrib(toggle_pd_imgOverlay, 'data-active-overlay');
-        toggle_attrib(toggle_activeOverlay, 'data-active-body-overlay');
-    });
-});
-toggle_activeOverlay.addEventListener('click', () => {
-        document.body.classList.toggle('body-fixed');
-        toggle_attrib(toggle_pd_imgOverlay, 'data-active-overlay');
-        toggle_attrib(toggle_activeOverlay, 'data-active-body-overlay');
-});
-
 
 //Product detail area
 //Button for the add to cart and incrementing the 
