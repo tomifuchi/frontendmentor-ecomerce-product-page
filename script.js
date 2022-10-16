fe93f65bd0a7fbf9576c612586741a6130d69b5f
tomifuchi <tomifuchi@yahoo.com>
2022-10-16 15:22:34 +0700

    Active box in progress

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
 //Product image area
-//Navigating the using thumbnail image below main image
-const pd_img_detail = document.querySelectorAll('.product-img-detail li');
-const pd_img_variation = document.querySelectorAll('.product-img-variation li');
+//Initializing
+const pd_img_detail = document.querySelectorAll('#img-detail .product-img-detail li');
+const pd_img_variation = document.querySelectorAll('#img-detail .product-img-variation li');
 
-//Initial scroll to the active image
-pd_img_detail[
-    [...pd_img_detail].indexOf(document.querySelector('.product-img-detail li[data-active-image]'))
-].scrollIntoView();
-
-pd_img_variation.forEach( img => {
+//If click or normal state image, go to zoomed state
+const body_dark_overlay = document.querySelector('.body-dark-overlay');
+pd_img_detail.forEach( img => {
     img.addEventListener('click', () => {
-        //Scroll to image, not necessarily through each image.
-        let currentIndex = [...pd_img_variation].indexOf(img);
-        delete document.querySelector('.product-img-variation li[data-active-overlay]').dataset.activeOverlay;
-        delete document.querySelector('.product-img-detail li[data-active-image]').dataset.activeImage;
-        img.dataset.activeOverlay = true;
-        pd_img_detail[currentIndex].dataset.activeImage = true;
-        pd_img_detail[currentIndex].scrollIntoView(); //Huehuehuehue
-    })
+        document.body.classList.toggle('lock-body');
+        body_dark_overlay.dataset.active = true;
+    });
 });
 
-//Button on the detail image itself
-const pd_img_detail_button_next = document.querySelector('.product-img-detail button[data-button-next]');
-const pd_img_detail_button_prev = document.querySelector('.product-img-detail button[data-button-prev]');
-function scroll_img_carousel(step) {
-   const active_img = document.querySelector('.product-img-detail li[data-active-image]');
+const active_box_close_btn = document.getElementById('close-btn');
+active_box_close_btn.addEventListener('click', () => {
+    document.body.classList.toggle('lock-body');
+    delete body_dark_overlay.dataset.active;
+});
+
+function add_event(id) {
+    const pd_img_detail = document.querySelectorAll(`${id} .product-img-detail li`);
+    const pd_img_variation = document.querySelectorAll(`${id} .product-img-variation li`);
+
+    pd_img_detail[
+        [...pd_img_detail].indexOf(document.querySelector(`${id} .product-img-detail li[data-active-image]`))
+    ].scrollIntoView();
+
+    //Initial scroll to the active image
+    pd_img_variation.forEach( img => {
+         img.addEventListener('click', () => {
+            //Scroll to image, not necessarily through each image.
+            let currentIndex = [...pd_img_variation].indexOf(img);
+            delete document.querySelector(`${id} .product-img-variation li[data-active-overlay]`).dataset.activeOverlay;
+            delete document.querySelector(`${id} .product-img-detail li[data-active-image]`).dataset.activeImage;
+            img.dataset.activeOverlay = true;
+            pd_img_detail[currentIndex].dataset.activeImage = true;
+            pd_img_detail[currentIndex].scrollIntoView(); //Huehuehuehue
+        });
+    });
+}
+add_event('#img-detail');
+add_event('#active-box');
+
+//Detail on active box
+const pd_img_detail_button_next = document.querySelector('#active-box .product-img-detail button[data-button-next]');
+const pd_img_detail_button_prev = document.querySelector('#active-box .product-img-detail button[data-button-prev]');
+
+function scroll_img_carousel(id, step) {
+   const active_img = document.querySelector(`${id} .product-img-detail li[data-active-image]`);
+   const pd_img_detail = document.querySelectorAll(`${id} .product-img-detail li`);
+   const pd_img_variation = document.querySelectorAll(`${id} .product-img-variation li`);
    let currentIndex = [...pd_img_detail].indexOf(active_img);
    let nextIndex;
 
@@ -37,87 +62,41 @@ function scroll_img_carousel(step) {
     nextIndex = currentIndex + step;
    }
 
-   delete document.querySelector('.product-img-variation li[data-active-overlay]').dataset.activeOverlay;
-   delete document.querySelector('.product-img-detail li[data-active-image]').dataset.activeImage;
+   delete document.querySelector(`${id} .product-img-detail li[data-active-image]`).dataset.activeImage;
+   delete document.querySelector(`${id} .product-img-variation li[data-active-overlay]`).dataset.activeOverlay;
    pd_img_variation[nextIndex].dataset.activeOverlay = true;
    pd_img_detail[nextIndex].dataset.activeImage = true;
    pd_img_detail[nextIndex].scrollIntoView();
 }
-
-pd_img_detail_button_next.addEventListener('click', () => {scroll_img_carousel(1);});
-pd_img_detail_button_prev.addEventListener('click', () => {scroll_img_carousel(-1);});
-
+pd_img_detail_button_next.addEventListener('click', () => {scroll_img_carousel('#active-box', 1);});
+pd_img_detail_button_prev.addEventListener('click', () => {scroll_img_carousel('#active-box', -1);});
 
 window.addEventListener('keyup', event => {
-    switch(event.key) {
-        case 'ArrowRight':
-            scroll_img_carousel(1);
-            break;
-        case 'ArrowLeft':
-            scroll_img_carousel(-1);
-            break;
+    const state = document.querySelector('.body-dark-overlay').hasAttribute('data-active');
+    if(state){
+        switch(event.key) {
+            case 'ArrowRight':
+                scroll_img_carousel('#active-box', 1);
+                break;
+            case 'ArrowLeft':
+                scroll_img_carousel('#active-box', -1);
+                break;
+            case 'Escape':
+                active_box_close_btn.dispatchEvent(new Event('click'));
+                break;
+        }
+    }
+    else{
+        switch(event.key) {
+            case 'ArrowRight':
+                scroll_img_carousel('#img-detail', 1);
+                break;
+            case 'ArrowLeft':
+                scroll_img_carousel('#img-detail', -1);
+                break;
+        }
     }
 });
-
-//---Zoomed active box area for the image detail zoomed in----
-/*
-pd_img_detail.forEach( img => {
-    img.addEventListener('click', () => {
-        if(document.getElementById('active-box')){
-            document.body.removeChild(document.getElementById('active-box'));
-            document.body.classList.toggle('lock-body');
-        } else {
-            const pd_img_active_box = document.querySelector('.product-img').cloneNode(true);
-            pd_img_active_box.setAttribute('id', 'active-box');
-            pd_img_active_box.style.cssText = '\
-                position: fixed; \
-                max-width: 500px;\
-                height: 100%;\
-                inset: 0;\
-                border: 1px solid black;\
-                z-index: 2;\
-                display: flex;\
-                justify-content: center;\
-                align-items: center;\
-            ';
-            pd_img_active_box.querySelectorAll('button[data-button-next], button[data-button-prev]').forEach(button => {
-                button.style.display = 'block';
-            });
-
-            document.body.insertBefore(pd_img_active_box, document.body.firstChild);
-            document.body.classList.toggle('lock-body');
-        }
-    });
-});
-*/
-
-/*
-const pd_img_active_box = document.querySelector('.product-img').cloneNode(true);
-pd_img_active_box.setAttribute('id', 'active-box');
-pd_img_active_box.style.cssText = '\
-    position: fixed; \
-    max-width: 500px;\
-    height: 100%;\
-    inset: 0;\
-    border: 1px solid black;\
-    z-index: -2;\
-    opacity: 0;\
-    display: flex;\
-    justify-content: center;\
-    align-items: center;\
-    transition: 300ms;\
-';
-
-pd_img_active_box.querySelectorAll('button[data-button-next], button[data-button-prev]').forEach(button => {
-    button.style.display = 'block';
-});
-document.body.insertBefore(pd_img_active_box, document.body.firstChild);
-
-pd_img_detail.forEach( img => {
-    img.addEventListener('click', () => {
-    });
-});
-*/
 
 //Product detail area
 //Button for the add to cart and incrementing the 
